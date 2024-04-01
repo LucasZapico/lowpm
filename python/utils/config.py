ea752af39f8ef33d89a7b6c9841be05416df90ca
Lucas Zapico <dev@lucaszapico.space>
2024-04-01 15:58:32 -0700

    feat: handle project paths other then current working dir

--- python/utils/config.py
+++ python/utils/config.py
@@ -1,17 +1,15 @@
-
 import os
 import yaml
 
 # Get the project root directory
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-default = {
-     'config_path': os.path.join(project_root, 'lowpm.config.yml')
-}
+default = {"config_path": os.path.join(project_root, "lowpm.config.yml")}
+
 
 def load_config():
     # Define the path of the config file
-    config_path = default['config_path']
+    config_path = default["config_path"]
 
     # Check if the config file exists
     if not os.path.exists(config_path):
@@ -19,7 +17,28 @@ def load_config():
         return None
 
     # Load the config file
-    with open(config_path, 'r') as file:
+    with open(config_path, "r") as file:
         config = yaml.safe_load(file)
 
     return config
+
+
+def check_and_create_project_dir():
+    # Load the config
+    config = load_config()
+
+    # Check if the config is None or if 'project_dir' is not in the config
+    if config is None or "project_dir" not in config:
+        print("Error: 'project_dir' not found in config.")
+        return
+
+    # Get the project directory
+    project_dir = config["project_dir"]
+
+    # Check if the project directory exists
+    if not os.path.exists(project_dir):
+        # If the project directory doesn't exist, create it
+        os.makedirs(project_dir)
+        print(f"Created project directory: {project_dir}")
+    else:
+        print(f"Project directory already exists: {project_dir}")
