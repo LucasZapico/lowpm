cb253d92ea885648cbe7b0ff20f3117add334c14
Lucas Zapico <dev@lucaszapico.space>
2024-04-03 04:58:52 -0700

    feat add loggers
    - this will be used to debug
    - enable feedback to the user for input errors

--- python/utils/logger.py
+++ python/utils/logger.py
@@ -10,23 +10,24 @@ if not os.path.exists('logs'):
 # Create a custom logger
 logger = logging.getLogger(__name__)
 
-# Set the level of logger to DEBUG
-logger.setLevel(logging.DEBUG)
 
 # Create handlers
-warn_handler = logging.FileHandler('logs/warning.log') # logging.StreamHandler()
-
+console_handler = logging.StreamHandler()
+warn_handler = logging.FileHandler('logs/warning.log') # 
 dump_handler = logging.FileHandler('logs/dump.log')
 error_handler = logging.FileHandler('logs/error.log')
 info_handler = logging.FileHandler('logs/info.log')
 # Set the level of handlers
+console_handler.setLevel(logging.DEBUG) 
 info_handler.setLevel(logging.INFO)
 warn_handler.setLevel(logging.WARNING)
 error_handler.setLevel(logging.ERROR)
 dump_handler.setLevel(logging.DEBUG)
 
+
 # Create formatters and add it to handlers
 # Create formatters and add it to handlers
+console_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
 dump_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
 warn_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
 error_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
@@ -35,6 +36,7 @@ info_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name
 
 
 # Add the formatter to the handlers
+console_handler.setFormatter(console_format)
 dump_handler.setFormatter(dump_format)
 warn_handler.setFormatter(warn_format)
 error_handler.setFormatter(error_format)
@@ -45,3 +47,4 @@ logger.addHandler(dump_handler)
 logger.addHandler(warn_handler)
 logger.addHandler(error_handler)
 logger.addHandler(info_handler)
+logger.addHandler(console_handler)
