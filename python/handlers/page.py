9d937ca7ad58cf5358a93976381f1892b79f49a2
Lucas Zapico <dev@lucaszapico.space>
2024-04-04 07:04:58 -0700

    feat reduce function that handle page events into sperate file

--- python/handlers/page.py
+++ python/handlers/page.py
@@ -1,49 +1,36 @@
 from bs4 import BeautifulSoup
+import os
 
+from utils.config import load_config
 from utils.utils_markdown import get_frontmatter, html_to_md, md_to_html
 from utils.logger import logger
 from utils.doc_utils import update_doc, make_doc
+from utils.misc import format_doc_file_name
+from handlers.board import get_boards_paths
+from handlers.board import update_board_columns, get_boards_paths
 
+# comon
+config = load_config()
+project_dir = os.path.abspath(config["project_dir"])
+board_columns = config["board_columns"]
 
-def page_change_handler(path):
-    page_frontmatter = get_frontmatter(path)
-    html_content = md_to_html(path)
+def page_change_handler(page):
+
+    page_frontmatter = page["frontmatter"]
+    html_content = page["html"]
+    page_file_name = page["file_name"]
+    page_path = page["path"]
+    page_title = page["title"]
+    
     # check file type 
     # check the page status is in the correct column on the board
     # parse the board markdown file and check if the page is in the correct column
     board_paths = get_boards_paths(page_frontmatter)
 
-    board_foo = board_paths[0]
-
-    board_foo_frontmatter = get_frontmatter(board_foo)
-    # get the html content of the board doc
-    board_foo_html = md_to_html(board_foo)
-    
-    soup = BeautifulSoup(board_foo_html, "html.parser")
-    
-    # get html node of the pages current status 
-    status_node = soup.find(lambda tag: tag.string == page_frontmatter["status"])
-
-    #  check if a given column has children
-    if status_node and not any(n for n in status_node if n.name):
-        print("status_node does not have child nodes or does not exist")
-        # if empty append page as list element to column
-        # new_list = soup.new_tag("ul")
-        new_list_item = soup.new_tag("li")
-        new_list_item.string = f"{page_frontmatter["title"]}.md"
-        # new_list.append(new_list_item)
-        status_node.append(new_list_item)
-    else:
-        print("status_node has child nodes")
-        # check list of status nodes for page if empty append page as list element to column
-
-        # chekc other list and remove if page is in it
-        
-    board_body_md = str(html_to_md(soup.prettify()))
+    for board_path in board_paths:
+        board_frontmatter = get_frontmatter(board_path)
+        # get the html content of the board doc
+        board_html = md_to_html(board_path)
+        update_board_columns(board_path, board_frontmatter, board_html, page_frontmatter )
 
     
-
-    updated_board = make_doc(board_body_md, board_foo_frontmatter)
-    
-    # update_board(board_foo, updated_board)
-    update_doc(f"{project_dir}/bar.board.md", str(updated_board))
