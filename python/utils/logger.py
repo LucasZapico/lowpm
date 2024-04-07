ef1d75126eefbfcaa4ca70470938fec933a669fb
Lucas Zapico <dev@lucaszapico.space>
2024-04-07 08:07:12 -0700

    feat(logs) logger creation handled in a simple generate logger func

--- python/utils/logger.py
+++ python/utils/logger.py
@@ -1,50 +1,48 @@
 import logging
 import os
-
+import sys
+from rich.logging import RichHandler
+from utils.colorized_util import console
 
 # Check if the directory exists
-if not os.path.exists('logs'):
+if not os.path.exists("logs"):
     # If the directory does not exist, create it
-    os.makedirs('logs')
+    os.makedirs("logs")
 
 # Create a custom logger
 logger = logging.getLogger(__name__)
 
+log_default_format = "%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s"
 
-# Create handlers
-console_handler = logging.StreamHandler()
-warn_handler = logging.FileHandler('logs/warning.log') # 
-dump_handler = logging.FileHandler('logs/dump.log')
-error_handler = logging.FileHandler('logs/error.log')
-info_handler = logging.FileHandler('logs/info.log')
+
+
+def generate_logger(path=None, level=logging.DEBUG, log_format=log_default_format):
+    if path:
+        log_handler = logging.FileHandler(path)
+    else:
+        log_handler = logging.StreamHandler()
+    # set log level 
+    log_handler.setLevel(level)
+    # set log format
+    log_handler.setFormatter(log_format)
+    # add log handler to logger
+    logger.addHandler(log_handler)
+
+
 # Set the level of handlers
-console_handler.setLevel(logging.DEBUG) 
-info_handler.setLevel(logging.INFO)
-warn_handler.setLevel(logging.WARNING)
-error_handler.setLevel(logging.ERROR)
-dump_handler.setLevel(logging.DEBUG)
-
-
-# Create formatters and add it to handlers
-# Create formatters and add it to handlers
-console_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
-dump_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
-warn_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
-error_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
-info_format = logging.Formatter('%(asctime)s [%(thread)d] %(levelname)-5s %(name)s - %(message)s')
-
-
-
-# Add the formatter to the handlers
-console_handler.setFormatter(console_format)
-dump_handler.setFormatter(dump_format)
-warn_handler.setFormatter(warn_format)
-error_handler.setFormatter(error_format)
-info_handler.setFormatter(info_format)
-
-# Add handlers to the logger
-logger.addHandler(dump_handler)
-logger.addHandler(warn_handler)
-logger.addHandler(error_handler)
-logger.addHandler(info_handler)
-logger.addHandler(console_handler)
+if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
+    # Create handlers
+    console.print("frozen")
+    # dump_handler = logging.FileHandler("logs/.log")
+    # console_handler.setLevel(logging.CRITICAL)
+else:
+    generate_logger(path="logs/info.log", level=logging.INFO)
+    # generate_logger(path="logs/warmomg.log", level=logging.WARNING)
+    # generate_logger(path="logs/error.log", level=logging.ERROR)
+    logging.StreamHandler()
+    # dump logger for check
+    # dump_handler = logging.FileHandler("logs/dump.log")
+    # dump_handler.setLevel(logging.DEBUG)
+    # logger.addHandler(dump_handler)
+    # Set the level of handlers
+    # console_handler.setLevel(logging.DEBUG)
