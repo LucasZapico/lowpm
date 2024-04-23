8a9c920e791d9c2990b06f7343938b272a8a3bba
Lucas Zapico <dev@lucaszapico.space>
2024-04-23 06:16:39 -0700

    feat update new args, - tp: type - tplt: template

--- python/_main.py
+++ python/_main.py
@@ -5,6 +5,7 @@ import yaml
 from utils.config import load_config, check_and_create_project_dir
 from app import initialize, config
 from utils.colorized_util import console
+
 # TODO: review, maybe consolidate the handlers into one file
 from handlers.init_handler import handle_init
 from handlers.cli_args import handle_new
@@ -12,11 +13,10 @@ from watchers import watch_directory
 
 
 def main():
-    
+
     # preloading and initializing the app
     initialize()
     # Load the config file
-    
 
     if not config:
         print("Exiting...")
@@ -37,14 +37,27 @@ def main():
     # init command
     init_parser = subparsers.add_parser("init", help="Init lowpm in current directory")
     init_parser.add_argument(
-        "--global", "-g",   action="store_true",  help="Init lowpm globally in user's home directory"
+        "--global",
+        "-g",
+        action="store_true",
+        help="Init lowpm globally in user's home directory",
     )
 
     new_parser = subparsers.add_parser("new", help="New project")
-    new_parser.add_argument("--template", help="Path to unqiue template")
-    new_parser.add_argument("--type", help="Type, board, list, page")
-    new_parser.add_argument("--name", help="Name of the new project")
-    new_parser.add_argument("--path", help="Path where the new project will be created")
+    new_parser.add_argument(
+        "--template", "-tplt", help="Path to unqiue template"
+    )
+    new_parser.add_argument(
+        "--type", "-tp", help="Type, board, list, page"
+    )
+    new_parser.add_argument(
+        "--name", "-n", help="Name of the new project"
+    )
+    new_parser.add_argument(
+        "--path",
+        "-p",
+        help="Path where the new project will be created",
+    )
 
     args = parser.parse_args()
 
@@ -56,15 +69,17 @@ def main():
         )
     elif args.command == "watch":
         watch_directory(config["project_dir"])
-    elif args.command == "init": 
-      # handler for "init" command
-      handle_init(args)
+    elif args.command == "init":
+        # handler for "init" command
+        handle_init(args)
     elif args.command == "new":
-      # handler for "new" command
-      handle_new(args)
+        # handler for "new" command
+        handle_new(args)
 
     else:
-        console.print(f"[cyan bold]lowpm[/cyan bold] doesn recongize the command: {args.command}\n[bold]Common commands:[/bold]\n[cyan]lowpm init[/cyan]: inits a lowpm in current dir allowing project specific config\n[cyan]lowpm new ---type page | board | list[/cyan]: create a new doc from template\n[cyan]lowpm --help[/cyan]: to see the available commands")
+        console.print(
+            f"[cyan bold]lowpm[/cyan bold] doesn recongize the command: {args.command}\n[bold]Common commands:[/bold]\n[cyan]lowpm init[/cyan]: inits a lowpm in current dir allowing project specific config\n[cyan]lowpm new ---type page | board | list[/cyan]: create a new doc from template\n[cyan]lowpm --help[/cyan]: to see the available commands"
+        )
 
 
 if __name__ == "__main__":
