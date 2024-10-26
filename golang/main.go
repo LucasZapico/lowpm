40a4ed9c666fdc27b5c1809373d04d3da43fdd08
LucasZapico <hello@lucaszapico.space>
2024-10-25 20:16:13 -0700

    feat: add main
    - dummied for right now

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,21 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
-import "fmt"
+import (
+	// "lowpm/cmd"
+	"lowpm/libs/utils"
+	// "fmt"
+)
 
 func main() {
-    fmt.Println("Hello, world!")
+	utils.PrintInfo("hello from lowpm")
+	// cmd.Execute()
+	// root :=	utils.GetCurrentUserRoot()
+	// utils.PrintInfo(root)
+	// Current returns the current user, or nil if not set.
+
+	// defer os.RemoveAll(path)
+	utils.PrintInfo("------- Done -------")
 }
