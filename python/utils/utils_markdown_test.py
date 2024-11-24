4f51fb4fbdd575661552314de49fe6b90a2bf430
LucasZapico <hello@lucaszapico.space>
2024-11-23 21:41:31 -0800

    test(utils): add test for markdown utils

--- python/utils/utils_markdown_test.py
+++ python/utils/utils_markdown_test.py
@@ -1,35 +1,35 @@
-import pytest 
+import pytest
 from utils.utils_markdown import md_to_html, get_frontmatter, html_to_md
 from utils.colorized_cli_utils import print_info
+from helpers.init_helpers import current_dir
 
-test_doc_path = "../test/test_utils_markdown_test.md"
+test_doc_path = f"{current_dir}/test/test_utils_markdown_test.md"
+
 
 ## take path to markdown file and return frontmatter
 def test_get_frontmatter():
     """test: function to get frontmatter from markdown file"""
-    print_info('--- test: parse frontmatter from md ---')
+    print_info("--- test: parse frontmatter from md ---")
     frontmatter = get_frontmatter(test_doc_path)
     print_info(frontmatter)
-    assert frontmatter == {'title': 'test md doc'}
+    assert frontmatter == {"title": "test md doc"}
 
 
 # ## take in raw html and return markdown
 ## TODO: (discovery) three \n after h1 ???
 # # TODO: validate
 def test_html_to_md():
-    """test: function to convert html to markdwon """
-    print_info('--- test: function to convert html to markdwon ---')
+    """test: function to convert html to markdwon"""
+    print_info("--- test: function to convert html to markdwon ---")
     html = html_to_md("<h1>Hello</h1>\n<p>I'm a test md doc</p>")
     print_info(repr(html))
     print_info(repr(f"# Hello\n\n\nI'm a test md doc\n\n"))
     assert repr(html) == repr("# Hello\n\n\nI'm a test md doc\n\n")
-    
 
 
 # take in markdown path and get the html content
 def test_md_to_html():
     """test: function to convert markdown to html"""
-    print_info('--- test: function to convert markdown to html ---')
-    content = md_to_html(test_doc_path)    
+    print_info("--- test: function to convert markdown to html ---")
+    content = md_to_html(test_doc_path)
     assert content == f"<h1>Hello</h1>\n<p>I'm a test md doc</p>"
-
